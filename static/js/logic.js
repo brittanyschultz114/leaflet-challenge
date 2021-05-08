9bc34e647eb851db4dadeac30900a7f4ec7eb041
unknown <brittanyschultz114@gmail.com>
2021-05-08 10:32:48 -0400

    updated logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -48,26 +48,6 @@ d3.json(plateLink,function(response){
 });
 
 function createMap(plates,earthQuakes) {
-    // var satellite = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    //     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    //     maxZoom: 18,
-    //     id: "mapbox.satellite",
-    //     accessToken: API_KEY
-    // });
-
-    // var grayscale = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    //     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    //     maxZoom: 18,
-    //     id: "mapbox.light",
-    //     accessToken: API_KEY
-    // });
-
-    // var outdoors = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    //     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    //     maxZoom: 18,
-    //     id: "mapbox.outdoors",
-    //     accessToken: API_KEY
-    // });
     var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", { 
         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
         tileSize: 512,
@@ -75,21 +55,21 @@ function createMap(plates,earthQuakes) {
         zoomOffset: -1,
         id: "mapbox/outdoors-v11",
         accessToken: API_KEY
-      });
+    });
     
-      var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
         maxZoom: 18,
         id: "satellite-v9",
         accessToken: API_KEY
-      });
+    });
     
-      var grayscale = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    var grayscale = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
         maxZoom: 18,
         id: "dark-v10",
         accessToken: API_KEY
-      });
+    });
 
     // Define a baseMaps object to hold our base layers
     var baseMaps = {
@@ -134,11 +114,10 @@ function createMap(plates,earthQuakes) {
 
 }
 
-
 function chooseColor(mag){
     switch(true){
         case (mag<1):
-            return "chartreuse";
+            return "LightGreen";
         case (mag<2):
             return "greenyellow";
         case (mag<3):
@@ -146,16 +125,16 @@ function chooseColor(mag){
         case (mag<4):
             return "DarkOrange";
         case (mag<5):
-            return "Peru";
-        default:
             return "red";
+        default:
+            return "DarkRed";
     };
 }
 
 function displayLegend(){
     var legendInfo = [{
         limit: "Mag: 0-1",
-        color: "chartreuse"
+        color: "LightGreen"
     },{
         limit: "Mag: 1-2",
         color: "greenyellow"
@@ -167,10 +146,10 @@ function displayLegend(){
         color:"DarkOrange"
     },{
         limit:"Mag: 4-5",
-        color:"Peru"
+        color:"red"
     },{
         limit:"Mag: 5+",
-        color:"red"
+        color:"DarkRed"
     }];
 
     var header = "<h3>Magnitude</h3><hr>";
@@ -178,8 +157,8 @@ function displayLegend(){
     var strng = "";
 
     for (i = 0; i < legendInfo.length; i++){
-        strng += "<p style = \"background-color: "+legendInfo[i].color+"\">"+legendInfo[i].limit+"</p> ";
-    }
+        strng += "<p style = \"background-color: "+legendInfo[i].color+"\">"+legendInfo[i].limit+"</p> "
+    };
     
     return header+strng;
 
